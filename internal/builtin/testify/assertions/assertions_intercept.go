50f02ac92e8b5070dd756a3dfb08faf62b13d006
Mihail Stoykov <M.Stoikov@gmail.com>
2024-06-10 12:43:44 +0300

    Change to using sobek instead of goja
    We are moving to a fork of goja under grafana org called sobek.
    
    More info in:
    - https://github.com/grafana/k6/issues/3772
    - https://github.com/grafana/k6/issues/3773

--- internal/builtin/testify/assertions/assertions_intercept.go
+++ internal/builtin/testify/assertions/assertions_intercept.go
@@ -10,11 +10,12 @@
 package assertions
 
 import (
-  "fmt"
-	assert "github.com/stretchr/testify/assert"
+	"fmt"
 	http "net/http"
 	url "net/url"
 	time "time"
+
+	assert "github.com/stretchr/testify/assert"
 )
 
 func (a *Assertions) Condition(comp assert.Comparison, msgAndArgs ...interface{}) bool {
